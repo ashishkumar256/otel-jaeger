1aaea9fbb53adb2981100b0d4d2ed38c796819dd
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2025-11-22 20:34:44 +0530

    debug

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('hello', views.hello),
     path('status', views.health_check),
     path('api/sunspot', views.sunspot_view),
-    path('api/timeout', views.redis_timeout),
-    path('api/delay/<str:delay>', views.latency),
     path('api/factorial/<str:n>', views.factorial),
+    path('api/delay/<str:delay>', views.latency),
+    path('api/timeout', views.redis_timeout),
+    path('api/crash', views.div_zero),
     # re_path(r'^api/factorial/(?P<n>-?\d+)/?$', views.factorial),
 ]
